53db39fe5c620a28ff9327ffef6ec000b3b94a26
Mary Jack <maryjackinthebox@gmail.com>
2018-03-24 23:02:27 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -2,7 +2,8 @@
 board = ["X", " ", " ", " ", " ", " ", " ", " ", "O"]
 puts "Enter a position on the board from 1-9:"
 user_input = gets.chomp
-number_entered = user_input
+number_entered = user_input.to_i - 1
+
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
 def valid_move?
