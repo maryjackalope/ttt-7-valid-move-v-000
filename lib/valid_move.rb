ce040573f0de438217fccc1138734811a1ed4d16
Mary Jack <maryjackinthebox@gmail.com>
2018-03-24 23:03:48 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -5,7 +5,7 @@ user_input = gets.chomp
 number_entered = user_input.to_i - 1
 
 def valid_move? (number_entered, board)
-  number_entered.between?(0, 8) 
+  number_entered.between?(0, 8) && !()
   
 
 
